155b49a2f22102ec63823d0cc86ac4f07542902f
Jeff Smith <jeff.r.smith@gmail.com>
2012-09-10 13:21:32 -0700

    add initial Wine domain class

---
+++ grails-app/domain/org/coenraets/cellar2/Wine.groovy
@@ -0,0 +1,15 @@
+package org.coenraets.cellar2
+
+class Wine {
+
+    String name
+    String grapes
+    String country
+    String region
+    String year
+    String picture
+    String description
+
+    static constraints = {
+    }
+}
