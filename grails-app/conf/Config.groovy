e3e3d7538dc673a2403ded427e6a4829104e83fb
Jeff Smith <jeff.r.smith@gmail.com>
2013-03-15 23:40:32 -0700

    wrap default groupId configuration in quotes

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -11,7 +11,7 @@
 //    grails.config.locations << "file:" + System.properties["${appName}.config.location"]
 // }
 
-grails.project.groupId = org.coenraets.cellar2 // change this to alter the default package name and Maven publishing destination
+grails.project.groupId = 'org.coenraets.cellar2' // change this to alter the default package name and Maven publishing destination
 grails.mime.file.extensions = true // enables the parsing of file extensions from URLs into the request format
 grails.mime.use.accept.header = false
 grails.mime.types = [
