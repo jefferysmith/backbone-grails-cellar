772012c0426b0a7f56ae3320743d7ba2ce506f4f
Jeff Smith <jeff.r.smith@gmail.com>
2013-08-27 16:34:15 -0700

    copy additional folders from client to webapp for build

--- cellar-client/Gruntfile.js
+++ cellar-client/Gruntfile.js
@@ -4,13 +4,17 @@ module.exports = function(grunt) {
     var serverWebapp = '../cellar-service/web-app'
 
     grunt.initConfig({
-        // make it easy to refer to properties within the package.json file
+        // make it easy to refer to properties defined inside the package.json file
         pkg: grunt.file.readJSON('package.json'),
 
         copy: {
             dev: {
                 files: [
-                    {src: 'app/images/**', dest: serverWebapp + '/images/'}
+                    {expand: true, cwd: 'app/images/',src: ['**'], dest: serverWebapp + '/images/'},
+                    {expand: true, cwd: 'app/lib/',src: ['**'], dest: serverWebapp + '/lib/'},
+                    {expand: true, cwd: 'app/scripts/',src: ['**'], dest: serverWebapp + '/scripts/'},
+                    {expand: true, cwd: 'app/styles/',src: ['**'], dest: serverWebapp + '/styles/'},
+                    {expand: true, cwd: 'app/templates/',src: ['**'], dest: serverWebapp + '/templates/'}
                 ]
             }
         },
@@ -28,6 +32,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-copy');
 
-    grunt.registerTask('dev', ['copy']);
+    grunt.registerTask('dev', ['copy:dev']);
     grunt.registerTask('default', ['jshint']);
 };
